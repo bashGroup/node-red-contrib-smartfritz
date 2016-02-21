d71564ff9ca9d22c3e6830388e337fef8db04b32
pedro <pedro@reboredo.de>
2016-02-21 21:21:02 +0100

    Testhalber weitere smartfritz-Aufrufe eingefügt. Funktionieren noch nicht.

--- smartfritz.js
+++ smartfritz.js
@@ -1,4 +1,8 @@
 var fritz = require('smartfritz')
+var retObject = {Sid:"0", Calls:"0",ListInfos:"0"};
+var sid;
+var calls;
+var listinfos;
 
 module.exports = function (RED) {
   function SmartFritzNode (n) {
@@ -10,15 +14,34 @@ module.exports = function (RED) {
 
 		node.on('input', function(msg) {
             try {
+                // AB HIER NEBENLAEUFIGKEITEN!!!
                 fritz.getSessionID(node.username, node.password, function(sid)
                 {
-                    console.log(sid);
+                    console.log("SID =" + sid);
+                    retObject.Sid = sid;
+                });
+
+                console.log("Fritz!Session ID: "+ sid);
+                fritz.getSwitchList(sid,function(listinfos){
+                    retObject.ListInfos = listinfos;
+                    console.log("Switches AIDs: "+listinfos);
+                });
+
+                fritz.getPhoneList(sid,function(calls){
+                    retObject.Calls = calls;
+                    console.log("Phone List: " + calls);
                 });
             } catch (e) {
                 console.log("Error!!")
                 console.log(e);
+            } finally {
+                msg.payload = retObject;
+                node.send(msg);
             }
+
+
         });
+
 
 	}
 	RED.nodes.registerType("smartfritz", SmartFritzNode);
